3aed2dd2993f127dd0b6055084c9dc5ad2680b8a
Balicholo <wisdombalicholo@gmail.com>
2024-06-04 11:05:47 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,113 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/* Menu show */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* Menu hidden */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const blurHeader = () =>{
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('blur-header') 
+                       : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
+
+//  EMAIL JS
+
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) =>{
+    e.preventDefault()
+
+    // SERVICE ID - TEMPLATE - #FORM - PUBLIC KEY
+    emailjs.sendForm('service_s1e94am', 'template_udqsghp', '#contact-form', 'NsKjurVRcXWkKzRfU')
+     .then(() =>{
+        // SHOW SENT MESSAGE
+        contactMessage.textContent = 'Message sent successfully ✅'
+
+        // REMOVE MESSAGE AFTER FIVE SECONDS
+        setTimeout(() =>{
+            contactMessage.textContent = ''
+        }, 5000)
+
+        // CLEAR FIELD
+        contactForm.requestFullscreen()
+
+     }, () =>{
+    //   SHOW ERROR MESSAGE
+    contactMessage.textContent = 'Message not sent(try WhatsApp) ❌'
+     })
+}
+
+contactForm.addEventListener('submit', sendEmail)
+
+// SHOW SCROLL UP
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+// SCROLL SECTIONS ACTIVE LINK
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
+
+// SCROLL REVEAL ANIMATION
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 1000,
+    delay: 200,
+    reset: true 
+    // animations repeat
+})
+
+sr.reveal(`.home__data, .home__social, .contact__container, .footer__container`)
+sr.reveal(`.home__image`, {origin: 'bottom'})
+sr.reveal(`.about__data, .skills__data`, {origin: 'left'})
+sr.reveal(`.about__image, .skills__content`, {origin: 'right'})
+sr.reveal(`.services__card, .projects__cards`, {interval: 200})
